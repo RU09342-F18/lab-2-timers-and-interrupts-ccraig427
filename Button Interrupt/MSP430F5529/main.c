b4ca463df3aa0e22a01a7015fa56c9b4f9d6726b
ccraig427 <35497783+ccraig427@users.noreply.github.com>
2018-09-25 23:56:40 -0400

    Update main.c

--- Button Interrupt/MSP430F5529/main.c
+++ Button Interrupt/MSP430F5529/main.c
@@ -8,12 +8,10 @@
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	P1DIR = BIT0;
-	P4DIR = BIT7;
-	P1REN = BIT1;
-	P1OUT = BIT1;
-	P4OUT = ~BIT1;
-	P1IE = BIT1;
+	P1DIR = BIT0; //Set P1.0 to output direction
+	P4DIR = BIT7; //Set P4.7 to output direction
+	P1REN = BIT1; //Enable Pull Up on SW2 (P1.1)
+	P1IE = BIT1; //P1.1 interrupt enabled
 	__enable_interrupt();
 	return 0;
 }
@@ -22,7 +20,7 @@ int main(void)
 __interrupt void Port_1(void)
 {
     __delay_cycles(500);
-    P1OUT ^= BIT0;
-    P4OUT ^= BIT7;
-    P1IFG = ~BIT1;
+    P1OUT ^= BIT0; //Toggle LED1
+    P4OUT ^= BIT7; //Toggle LED2
+    P1IFG = ~BIT1; //P1.1 IFG cleared
 }
