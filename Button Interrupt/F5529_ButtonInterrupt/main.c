4578f074d7e7676e41cf8460db61263de41c8c84
ccraig427 <35497783+ccraig427@users.noreply.github.com>
2018-09-28 15:08:20 -0400

    Update main.c

--- Button Interrupt/F5529_ButtonInterrupt/main.c
+++ Button Interrupt/F5529_ButtonInterrupt/main.c
@@ -1,18 +1,18 @@
 #include <msp430.h> 
 
 /**Date Created: 9/24/18
-   Date Last Edited:
+   Date Last Edited: 9/28/18
    Author: Colin Craig
    MSP430F5529 Button Interrupt
    */
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	P1DIR = BIT0;
-	P4DIR = BIT7;
-	P1REN = BIT1;
-	P4REN = ~BIT1;
-	P1IE = BIT1;
+	WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
+	P1DIR = BIT0; //Sets the direction of P1.0 to output
+	P4DIR = BIT7; //Sets the direction of P4.7 to output
+	P1REN = BIT1; //Adds a pull-up resistor to P1.1
+	P4REN = ~BIT1; //Adds a pull-down resistor to P4.1
+	P1IE = BIT1; //Enables interrupts for 
 	__enable_interrupt();
 	return 0;
 }
