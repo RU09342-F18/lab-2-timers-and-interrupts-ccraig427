585b4f228b98b5c1e3de197e2bbfc3723897a2e2
ccraig427 <35497783+ccraig427@users.noreply.github.com>
2018-09-28 15:45:34 -0400

    Update main.c

--- Button Interrupt/F5529_ButtonInterrupt/main.c
+++ Button Interrupt/F5529_ButtonInterrupt/main.c
@@ -1,26 +1,26 @@
-#include <msp430.h> 
-
 /**Date Created: 9/24/18
-   Date Last Edited:
+   Date Last Edited: 9/28/18
    Author: Colin Craig
    MSP430F5529 Button Interrupt
    */
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	P1DIR = BIT0; //Sets the direction of P1.0 to output
-	P4DIR = BIT7; //Sets the direction of P4.7 to output
-	P1REN = BIT1; //Adds a pull-up resistor to P1.1
-	P1IE = BIT1; //Enables interrupts to P1.1
-	__enable_interrupt();
-	return 0;
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    P1DIR = BIT0; //Sets the direction of P1.0 to output
+    P4DIR = BIT7; //Sets the direction of P4.7 to output
+    P1OUT = BIT0; //Sets the initial value of P1.0 On
+    P4OUT = ~BIT7; //Sets the initial value of P4.7 Off
+    P2REN = BIT1; //Adds a pull-up resistor to P2.1
+    P2IE = BIT1; //Enables interrupts to P2.1
+    __enable_interrupt();
+    return 0;
 }
 
-#pragma vector=PORT1_VECTOR
-__interrupt void Port_1(void)
+#pragma vector=PORT2_VECTOR
+__interrupt void Port_2(void)
 {
     __delay_cycles(500); //Delays 500 cycles to help with debouncing
     P1OUT ^= BIT0; //Toggles LED1
     P4OUT ^= BIT7; //Toggles LED2
-    P1IFG = ~BIT1; //Clears P1.1 interrupt flag
+    P2IFG &= ~BIT1; //Clears P2.1 interrupt flag
 }
